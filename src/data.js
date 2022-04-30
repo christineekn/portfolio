bdd9b7dab85b9e7f456082690d013a5b1484f51f
christineekn <koniou.christine@hotmail.com>
2022-04-30 16:48:18 +0200

    fixed ui project titles

--- src/data.js
+++ src/data.js
@@ -4,14 +4,14 @@ import Maynooth from "./img/maynooth.png";
 const projectUI = [
   {
     id: 1,
-    title: "Maynooth Furniture UI/UX Design App",
+    title: "Maynooth Furniture UI/UX App Design ",
     desc: "Final mobile app design - Udemy project",
     img: Maynooth,
     link: "https://www.behance.net/gallery/141911635/Maynooth-Furniture-UIUX-App-Design"
   },
   {
     id: 2,
-    title: "Roar Bikes UI/UX Design Website",
+    title: "Roar Bikes UI/UX Website Design ",
     desc: "Final website design - Udemy project",
     img: Roar,
     link: "https://www.behance.net/gallery/96187711/Roar-Bikes"
