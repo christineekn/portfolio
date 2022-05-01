70dea12adf407f88a603e51b24da780529eaef74
christineekn <koniou.christine@hotmail.com>
2022-05-01 11:37:23 +0200

    old navbar

--- src/components/Navbar/Topnav.js
+++ src/components/Navbar/Topnav.js
@@ -6,68 +6,68 @@ import "./Topnav.css";
 
 const Topnav = () => {
   return (
-    <Navbar bg="dark" variant={"dark"} expand="lg" fixed="top" className="t-nav">
-      <Container>
-        <Navbar.Brand href="#home" className="brand">Christine Koniou.</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" >
-          <Nav className="d-flex float-md-end">
-            <Nav.Link
-              className="p-2"
-              target="_blank"
-              href="https://www.behance.net/christinekon"
-            >
-              <BsBehance size={20} />
-            </Nav.Link>
-            <Nav.Link
-              className="p-2"
-              target="_blank"
-              href="https://dribbble.com/christKon"
-            >
-              <BsDribbble size={20} />
-            </Nav.Link>
-            <Nav.Link
-              className="p-2"
-              target="_blank"
-              href="https://github.com/christineekn"
-            >
-              <BsGithub size={20} />
-            </Nav.Link>
-            <Nav.Link
-              className="p-2"
-              target="_blank"
-              href="https://www.linkedin.com/in/christine-koniou-2a0044100/"
-            >
-              <BsLinkedin size={20} />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-
-    // <div className="t">
-    //   <Navbar expand="lg" className="p-2 container" fixed="top">
-    //     <Container>
-    //       <Navbar.Brand href="#home" className="brand">
-    //         Christine Koniou.
-    //       </Navbar.Brand>
-    //       <Nav className="list d-xs-none">
-    //         <Nav.Link className="p-2" target="_blank" href="https://www.behance.net/christinekon">
-    //           <BsBehance size={20}/>
+    // <Navbar bg="dark" variant={"dark"} expand="lg" fixed="top" className="t-nav">
+    //   <Container>
+    //     <Navbar.Brand href="#home" className="brand">Christine Koniou.</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav" >
+    //       <Nav className="d-flex float-md-end">
+    //         <Nav.Link
+    //           className="p-2"
+    //           target="_blank"
+    //           href="https://www.behance.net/christinekon"
+    //         >
+    //           <BsBehance size={20} />
     //         </Nav.Link>
-    //         <Nav.Link className="p-2" target="_blank" href="https://dribbble.com/christKon">
-    //         <BsDribbble size={20}/>
+    //         <Nav.Link
+    //           className="p-2"
+    //           target="_blank"
+    //           href="https://dribbble.com/christKon"
+    //         >
+    //           <BsDribbble size={20} />
     //         </Nav.Link>
-    //         <Nav.Link className="p-2" target="_blank" href="https://github.com/christineekn">
-    //           <BsGithub size={20}/>
+    //         <Nav.Link
+    //           className="p-2"
+    //           target="_blank"
+    //           href="https://github.com/christineekn"
+    //         >
+    //           <BsGithub size={20} />
     //         </Nav.Link>
-    //         <Nav.Link className="p-2" target="_blank" href="https://www.linkedin.com/in/christine-koniou-2a0044100/">
-    //           <BsLinkedin size={20}/>
+    //         <Nav.Link
+    //           className="p-2"
+    //           target="_blank"
+    //           href="https://www.linkedin.com/in/christine-koniou-2a0044100/"
+    //         >
+    //           <BsLinkedin size={20} />
     //         </Nav.Link>
     //       </Nav>
-    //     </Container>
-    //   </Navbar>
-    // </div>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
+
+    <div className="t">
+      <Navbar expand="lg" className="p-2 container" fixed="top">
+        <Container>
+          <Navbar.Brand href="#home" className="brand">
+            Christine Koniou.
+          </Navbar.Brand>
+          <Nav className="list d-xs-none">
+            <Nav.Link className="p-2" target="_blank" href="https://www.behance.net/christinekon">
+              <BsBehance size={20}/>
+            </Nav.Link>
+            <Nav.Link className="p-2" target="_blank" href="https://dribbble.com/christKon">
+            <BsDribbble size={20}/>
+            </Nav.Link>
+            <Nav.Link className="p-2" target="_blank" href="https://github.com/christineekn">
+              <BsGithub size={20}/>
+            </Nav.Link>
+            <Nav.Link className="p-2" target="_blank" href="https://www.linkedin.com/in/christine-koniou-2a0044100/">
+              <BsLinkedin size={20}/>
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
